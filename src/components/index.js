c4a30c8d8eff7c005dec25b2e61b6cea96e4f43f
sailikithsarabu <sailikith74@gmail.com>
2022-03-10 08:56:11 -0800

    index.jsis been with import corrections

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import PopMovies from "./PopMovies";
+import Results from "./Results";
+import TopRatedMovies from "./TopRatedMovies";
+import Info from "./Info";
+import About from "./About";
+
+export {
+  PopMovies,
+  TopRatedMovies,
+  Info,
+  Results,
+  About,
+};
